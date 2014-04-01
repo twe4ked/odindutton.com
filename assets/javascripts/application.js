885727ea51cbe8f72c2af4c5e8e56106e92226fb
Odin Dutton <odindutton@gmail.com>
2014-04-01 10:14:10 +0100

    Update JavaScript to match human API changes

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,7 +1,7 @@
 $(function() {
   $.get('http://human.odin.bz/location.json', function(data) {
     $('header h2 span').text('@ ' + data['name']);
-    $('header h2 span').attr('title', data['time']);
+    $('header h2 span').attr('title', data['source_created_at']);
   }).fail(function() {
     $('header h2 span').text('@ Earth');
   });
