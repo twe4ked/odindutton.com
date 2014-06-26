1e517720e9447c3a9fb85e5bd05575d5993aafc9
Odin Dutton <odindutton@gmail.com>
2014-06-26 19:12:09 +0200

    Add fancy hiding cover image

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -17,3 +17,12 @@ window.onload = function(event) {
   }
   request.send()
 }
+
+window.onscroll = function(event) {
+  if (window.cover_height == undefined) { window.cover_height = document.getElementById('cover').offsetHeight }
+  if (document.body.scrollTop <= window.cover_height) {
+    document.body.classList.remove('hide-cover')
+  } else {
+    document.body.classList.add('hide-cover')
+  }
+}
