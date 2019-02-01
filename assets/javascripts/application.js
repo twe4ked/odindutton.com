b3643daf6420aeeab1f1fed552d4e836ee0cfc4e
Odin Dutton <odindutton@gmail.com>
2019-02-01 14:01:29 +1100

    No longer show location
    This isn't available over HTTPS and I don't think it's that useful.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,23 +1,3 @@
-window.onload = function(event) {
-  span = document.querySelectorAll('header h2 span')[0]
-
-  request = new XMLHttpRequest()
-  request.open('GET', 'http://human.odin.bz/location.json')
-  request.onerror = function() {
-    span.textContent = '@ Earth'
-  }
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400){
-      data = JSON.parse(request.responseText)
-      span.textContent = '@ ' + data['name']
-      span.setAttribute('title', data['source_created_at'])
-    } else {
-      request.onerror()
-    }
-  }
-  request.send()
-}
-
 window.onscroll = function(event) {
   if (window.cover_height == undefined) { window.cover_height = document.getElementById('cover').offsetHeight }
   if (document.body.scrollTop <= window.cover_height) {
