6c166d937b3d2cfce08b03dffe80a445c5c02ce1
Odin Dutton <odindutton@gmail.com>
2020-04-27 09:28:41 +1000

    Remove fancy header scrolling

--- assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function(event) {
-  if (window.cover_height == undefined) { window.cover_height = document.getElementById('cover').offsetHeight }
-  if (document.body.scrollTop <= window.cover_height) {
-    document.body.classList.remove('hide-cover')
-  } else {
-    document.body.classList.add('hide-cover')
-  }
-}
